16a343ea36a08d0a20111d955ea20568d33e66a9
Carlos Alberto Martínez Gómez <alberto992015@gmail.com>
2023-10-06 15:09:01 -0600

    Mesasage
    Primer commit

---
+++ default/app/controllers/empresas_controller.php
@@ -0,0 +1,26 @@
+<?php
+ 
+class EmpresasController extends AppController
+{
+    public function index(){
+        $this->empresas = (new Empresas())->find();
+        
+    }
+
+    public function create(){
+        if(Input::hasPost('empresa')){
+            $empresa =new Empresas (Input::post('empresa'));
+            
+            if($empresa->create()){
+                Flash::valid('Empresa Guardada');
+                Input::delete();
+            }
+            else{
+                Flash::error('No se registro correctamente');
+                
+            }
+        }
+
+    }
+    
+}
